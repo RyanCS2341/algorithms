cb576488476b062ea9be4ed20918e435de28e422
ryanstanfield <stanfieldrcs@gmail.com>
2024-04-21 19:12:50 -0400

    refactor: remove comments

--- dsa/contains_sum.py
+++ dsa/contains_sum.py
@@ -18,31 +18,26 @@ def containsSum(nums: list[int], target: int) -> bool:
 class TestContainsSum(unittest.TestCase):
 
     def test_containsSum_success(self):
-        # Test 1
         nums = [1, 2, 4, 6, 8, 9, 14, 15]
         target = 13
         self.assertTrue(containsSum(nums, target))
 
     def test_containsSum_over(self):
-        # Test 2
         nums = [1, 2, 4, 6, 8, 9, 14, 15]
         target = 100
         self.assertFalse(containsSum(nums, target))
 
     def test_containsSum_empty(self):
-        # Test 3
         nums = []
         target = 5
         self.assertFalse(containsSum(nums, target))
 
     def test_containsSum_single(self):
-        # Test 4
         nums = [5]
         target = 5
         self.assertFalse(containsSum(nums, target))
 
     def test_containsSum_fail(self):
-        # Test 5
         nums = [1, 2, 3, 4, 4, 5, 6]
         target = 8
         self.assertTrue(containsSum(nums, target))
